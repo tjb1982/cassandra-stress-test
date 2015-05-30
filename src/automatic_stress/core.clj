2afa1d7968c7c912422004bc214cf616d7c2955c
Tom Brennan <tjb1982@gmail.com>
2015-05-30 17:06:59 -0400

    added assertion logic

--- src/automatic_stress/core.clj
+++ src/automatic_stress/core.clj
@@ -35,12 +35,59 @@
         ois (java.io.ObjectInputStream. bais)]
     (-> ois .readObject)))
 
+(defn non-negative
+  [value state assertion & _]
+  (let [state (if (nil? (:non-negative state))
+                (assoc state :non-negative {:count 0})
+                state)]
+    (if ((if (true? (first (val (first assertion)))) neg? pos?) value)
+      (assoc state :non-negative {:count (inc (-> state :non-negative :count))})
+      state)))
+
+(defn directional
+  [value state assertion & _]
+  (let [state (if (nil? (:directional state))
+                (assoc state :directional {:count 0 :prev nil})
+                state)
+        op (if (= "asc" (first (val (first assertion)))) > <)]
+    (assoc-in
+      (if (and (not (nil? (-> state :directional :prev)))
+               (op (-> state :directional :prev) value))
+        (update-in state [:directional :count] inc)
+        state)
+      [:directional :prev]
+      value)))
+
+(defn volatile
+  [value state assertion idx]
+  (let [state (if (nil? (:volatile state))
+                (assoc state :volatile {:count 0, :prev nil})
+                state)
+        freq (first (val (first assertion)))]
+    (if (= (mod idx freq) 0)
+      (assoc-in
+        (if (= (-> state :volatile :prev) value)
+          (update-in state [:volatile :count] inc)
+          state)
+        [:volatile :prev]
+        value)
+      state)))
+
 (defn record-attribute
-  ([session test-keyspace iteration jmx-host jmx-port attribute frequency finished]
-    (record-attribute session test-keyspace iteration jmx-host jmx-port attribute frequency finished 0))
-  ([session test-keyspace iteration jmx-host jmx-port attribute frequency finished idx]
-    (let [statement (BoundStatement. (-> session (.prepare (str
-                                                             "insert into " test-keyspace ".attributes ( "
+  ([session recording-keyspace iteration jmx-host jmx-port attribute frequency finished]
+    (record-attribute session recording-keyspace iteration jmx-host jmx-port attribute frequency finished 0 {}))
+  ([session recording-keyspace iteration jmx-host jmx-port attribute frequency finished idx state]
+    (let [value (with-connection {:host jmx-host :port jmx-port}
+                  (jmx/read (name (:object-name attribute)) (keyword (:attribute attribute))))
+          assertion-data (reduce
+                           (fn [state assertion]
+                             (if-let [fun (resolve (symbol (str "automatic-stress.core/" (name (ffirst assertion)))))]
+                               (fun value state assertion idx)
+                               state))
+                           state
+                           (-> attribute :assertions))
+          statement (BoundStatement. (-> session (.prepare (str
+                                                             "insert into " recording-keyspace ".attributes ( "
                                                              "  iteration,"
                                                              "  object_name,"
                                                              "  attribute,"
@@ -49,36 +96,34 @@
                                                              "  level,"
                                                              "  received"
                                                              ") values (?, ?, ?, ?, ?, ?, ?);"))))]
-      (with-connection
-        {:host jmx-host :port jmx-port}
-        (-> session (.execute (-> statement (.bind
-          (into-array Object
-            [iteration
-             (name (:object-name attribute))
-             (:attribute attribute)
-             "bigint"
-             (serialize-value (jmx/read (name (:object-name attribute)) (keyword (:attribute attribute))))
-             (int 0)
-             (.getTime (java.util.Date.))]))))))
+      (-> session (.execute (-> statement (.bind
+        (into-array Object
+          [iteration
+           (name (:object-name attribute))
+           (:attribute attribute)
+           "bigint"
+           (serialize-value value)
+           (int 0)
+           (.getTime (java.util.Date.))])))))
       (print ".") (flush)
       (Thread/sleep (or (:frequency attribute) frequency))
       (if (nil? @finished)
-        (recur session test-keyspace iteration jmx-host jmx-port attribute frequency finished (inc idx))
-        {:attribute attribute :queries idx}))))
+        (recur session recording-keyspace iteration jmx-host jmx-port attribute frequency finished (inc idx) assertion-data)
+        {:attribute attribute :records (inc idx) :assertion-data assertion-data}))))
 
 (defn maybe-create-test-schema
-  [session test-keyspace]
+  [session recording-keyspace]
   (-> session (.execute (str
-                          "create keyspace if not exists " test-keyspace
+                          "create keyspace if not exists " recording-keyspace
                           " with replication = "
                           "{ 'class':'SimpleStrategy', 'replication_factor':3};")))
   (-> session (.execute (str
-                          "create table if not exists " test-keyspace ".iterations ("
+                          "create table if not exists " recording-keyspace ".iterations ("
                           "  iteration uuid primary key,"
                           "  attributes set<text>"
                           ");")))
   (-> session (.execute (str
-                          "create table if not exists " test-keyspace ".attributes ("
+                          "create table if not exists " recording-keyspace ".attributes ("
                           "  iteration uuid,"
                           "  object_name text,"
                           "  attribute text,"
@@ -94,7 +139,10 @@
   (let [statement (BoundStatement.
                     (-> session
                       (.prepare
-                        (str "insert into " keyspace ".iterations (iteration, attributes) values (?,?);"))))]
+                        (str "insert into " keyspace ".iterations ("
+                             "  iteration,"
+                             "  attributes"
+                             ") values (?,?);"))))]
     (-> session
       (.execute
         (-> statement
@@ -110,34 +158,36 @@
   [properties-file & iteration]
 
   (let [properties (yaml/parse-string (slurp properties-file))
-        node-address (or (-> properties :tester-contact-point) "localhost")]
-    (if (cassandra-is-running? node-address)
-      (let [cluster (-> (Cluster/builder) (.addContactPoint (-> properties :recorder-contact-point)) .build)
-            test-keyspace (str (-> properties :test-name))
-            iteration (java.util.UUID/fromString (or iteration (str (java.util.UUID/randomUUID))))
+        tester-address (or (-> properties :tester-contact-point) "localhost")]
+    (if (cassandra-is-running? tester-address)
+      (let [cluster (-> (Cluster/builder)
+                      (.addContactPoint (-> properties :recorder-contact-point))
+                      .build)
+            recording-keyspace (str (-> properties :recording-keyspace))
+            iteration (java.util.UUID/fromString
+                        (or iteration (str (java.util.UUID/randomUUID))))
             attributes (-> properties :attributes)
             session (-> cluster .connect)]
 
-        (maybe-create-test-schema session test-keyspace)
-        (record-iteration session test-keyspace iteration attributes)
+        (maybe-create-test-schema session recording-keyspace)
+        (record-iteration session recording-keyspace iteration attributes)
 
-        (let [host (first (clojure.string/split node-address #":"))
+        (let [host (first (clojure.string/split tester-address #":"))
               finished (atom nil)
               record-promises (doall
                                 (pmap
                                   #(future
                                      (record-attribute session
-                                                       test-keyspace
+                                                       recording-keyspace
                                                        iteration
                                                        host
                                                        (-> properties :jmx-port)
                                                        %
-                                                       (-> properties :frequency)
+                                                       (or (-> properties :frequency) 1000)
                                                        finished))
                                   attributes))
-              stress-promise (future (sh "/home/tjb1982/nclk/them/datastax/apache-cassandra-2.0.15/tools/bin/cassandra-stress"))
-                             ;(future (sh "ls" "-la"))
-              ]
+              stress-promise (future (sh "ls"))] ;(apply sh (clojure.string/split (-> properties :test-invocation) #" ")))]
+          (Thread/sleep 1000)
           (swap! finished (fn [_] @stress-promise))
           (println "\n" (:out @finished))
           (doseq [p record-promises] (pprint/pprint @p))
